12e09385759a7f9f58004d98e42cdd19038a6293
Sachin Mittal <42431274+mittalsam98@users.noreply.github.com>
2022-01-15 17:47:57 +0530

    Added company name and animation working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
- import Sidebar from './components/sidebar/Sidebar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './components/sidebar/Sidebar';
 import About from './components/about/About';
 import Education from './components/Education/Education';
 import Interest from './components/skills/Interest'
@@ -10,32 +10,32 @@ import CarouselImages from './components/carousel/CarouselImages.js'
 
 class App extends Component {
 
-  render(){
-  return (
-    <Router>
-      <div className="App">
+  render() {
+    return (
+      <Router>
+        <div className="App">
           <div className="side">
             <nav className="navbar side navbar-expand-lg navbar-light p-0" >
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" style={{zIndex:'1'}}>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" style={{ zIndex: '1' }}>
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <Sidebar />
               </div>
-            </nav>  
+            </nav>
           </div>
-            <div className="main">
-              <div>
+          <div className="main">
+            <div>
 
-                 <CarouselImages />
-              </div>
-                 <About />
-                <Interest />
-                <Education />
+              <CarouselImages />
             </div>
-      </div>
-   </Router>
-  );
+            <About />
+            <Interest />
+            <Education />
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import classes from './About.module.css';
 import ScrollAnimation from 'react-animate-on-scroll';
-import "animate.css/animate.min.css"; 
+import "animate.css/animate.min.css";
 
 class About extends Component {
     render() {
         return (
             <div className={classes.box} id="about">
-                <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
-                    <span  className={classes.head}>ABOUT ME</span>
+                <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+                    <span className={classes.head}>ABOUT ME</span>
                     <h2 className={classes.heading}>Who Am I?</h2>
                     <div className={classes.About}>
-                        <p> My name is SACHIN MITTAL and I am currently pursuing my degree in Bachelor of Technology in Computer Science and Engineering from Sant Longowal Institute of Engineering and Technology. I want to be a developer as I am interested in developing new things which excite me a lot. :)  </p>
-                         <p className={classes.br}>I love exploring new technologies and being a practitioner, I like to stay on top of latest trends.
- </p>
+                        <p> My name is <b>SACHIN MITTAL</b> and I am currently working as Software Developer Engineer at <a target="_blank" href="https://www.springworks.in/"><b>Springworks</b></a>. I completed my degree in Bachelor of Technology in Computer Science and Engineering from Sant Longowal Institute of Engineering and Technology. I am much interested in developing new things which excite me a lot. :)  </p>
+                        <p className={classes.br}>I love exploring new technologies and being a practitioner, I like to stay on top of latest trends. I try to leave every line of code I write more readable, accessible, and modular.
+                        </p>
                     </div>
                 </ScrollAnimation>
             </div>
